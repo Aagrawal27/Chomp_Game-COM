f22042b0bc3fdd8de2acd76a3baf36a13f4343af
Aagrawal27 <aarav_agrawal27@milton.edu>
2025-05-06 09:04:46 -0400

    printing all boards

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -11,6 +11,28 @@ public class MyPlayer {
          * This code will run just once, when the game opens.
          * Add your code here.
          */
+        for (int x = 1; x <= 3; x++) {
+            for (int y = 0; y <= 3; y++) {
+                for (int z = 0; z <= 3; z++) {
+                    if (y <= x && z <= y) {
+                        System.out.println("Regular Board: "+String.valueOf(x) + String.valueOf(y) + String.valueOf(z));
+                        derivedBoards(x,y,z);
+                    }
+                }
+            }
+        }
+    }
+
+    public void derivedBoards(int i, int j, int k){
+
+        for(int x = 1; x<=i; x++){
+            for(int y = 0; y<=j; y++){
+                for(int z = 0; z<=k; z++){
+
+                    System.out.println(x+""+y+""+z);
+                }
+            }
+        }
     }
 
     public Point move(Chip[][] pBoard) {
@@ -30,15 +52,8 @@ public class MyPlayer {
          * You'll be returning a data type called Point which consists of two integers.
          */
         int[] boards = new int[19];
-        for(int x =0; x<=3; x++){
-            for(int y =0; y<=3; y++){
-                for(int z = 0; z<=3; z++){
-                    if(y<=x&&z<=y) {
-                        System.out.println(String.valueOf(x) + String.valueOf(y) + String.valueOf(z));
-                    }
-                }
-            }
-        }
+
+
 
         Point myMove = new Point(row, column);
         return myMove;
